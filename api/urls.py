b87a05c547cd9d0260beb00343e81798e2a3e713
Ben Lawson <bklawson@bsu.edu>
2019-02-15 14:32:35 -0500

    Added Class and Class Enrollment to the models and db. Removed forms from Demographic and Position creation. Restructured URL file to clean it up. Fixed logic bugs in the Views files.

--- api/urls.py
+++ api/urls.py
@@ -4,11 +4,22 @@ from .views import *
 
 urlpatterns = [
     path('', index),
-    path('login/', login),
-    path('register/', register),
-    path('logout/', logout),
+
+    # Authentication
+    path('login', login),
+    path('register', register),
+    path('logout', logout),
     path('reset_password/<str:reset_code>', reset_password),
     path('request_password_reset/<str:username>', request_password_reset),
-    path('demographic/', demographic_form),
-    path('position/', position_form)
+
+    # Demographic Requests
+    path('demographic/create', demographic_create),
+    path('demographic/update', demographic_update),
+    path('demographic/delete', demographic_delete),
+    path('demographic/select', demographic_select),
+
+    # Position Requests
+    path('position/create', position_create),
+    path('position/select/all', position_select_all),
+    path('position/select', position_select_id)
 ]
